627eac68f6e0f8033f5a80750153d4c8145e548b
firdosh Ali <firdoshali00000@gmail.com>
2024-07-27 19:03:30 +0545

    Initial commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request){
+        $validateUser = Validator::make(
+            $request->all(),
+            [
+                'name'=>'required',
+                'contact'=>'required',
+                'email'=>'required|email|unique:users,email',
+                'password'=>'required',
+            ]
+        );
+
+        if($validateUser->fails()){
+            return response()->json([
+                'status'=> false,
+                'message'=>'validation Error',
+                'errors'=> $validateUser->errors()->all()
+            ],401);
+        }
+
+        $user = User::create([
+            'name'=> $request->name,
+            'contact'=>$request->contact,
+            'email'=> $request->email,
+            'password'=> $request->password,
+        ]);
+
+        return response()->json([
+            'status'=> true,
+            'message'=> 'User created successfully',
+            'user'=> $user,
+        ],200);
+      }
+
+
+    public function login(Request $request){
+        $validateUser = validator::make(
+            $request->all(),
+            [
+                'email' =>'required|email',
+                'password'=>'required',
+            ]
+            );
+            
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Authentication failed',
+                    'errors'=> $validateUser->errors()->all()
+                ],404);
+            }
+
+            if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+                $authUser = Auth::user();
+                return response()->json([
+                    'status' => true,
+                    'message' =>'User logged in Successfully',
+                    'token' => $authUser->createToken("Api Token")->plainTextToken,
+                    'token_type' => 'bearer'
+                ],200);
+            }else{
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & passsword doesnnot match',
+                ],401);
+            }
+
+    }
+
+    public function logout(Request $request){
+
+        $user = $request->user();
+        $user->tokens()->delete();
+
+        return response()->json([
+            'status'=> true,
+            'user'=> $user,
+            'message'=> 'User loged out Successfully',
+        ],200);
+
+    }
+}
